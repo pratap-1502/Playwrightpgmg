8eb79d4a211947f05b422fe2525f6696fb0b21e1
Pratap <dvpratap40@email.com>
2026-06-12 14:36:29 +0530

    function scripts

---
+++ June12/fnwr.ts
@@ -0,0 +1,4 @@
+function square(a:number): number{
+    return a*a
+}
+console.log(square(33))
---
+++ June12/funwithparams.ts
@@ -0,0 +1,4 @@
+function fnwp(name: string): void {
+    console.log("Hello world with params"+name)
+}
+fnwp("Pratap")
