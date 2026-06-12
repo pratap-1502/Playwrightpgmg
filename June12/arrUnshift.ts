0b8f1116dc6dae8dbd6b8acff0f6eedeafee3c99
Pratap <dvpratap40@email.com>
2026-06-12 15:33:42 +0530

    shift and unshift

---
+++ June12/arrUnshift.ts
@@ -0,0 +1,6 @@
+let num12 = [23, 3, 42, 32, 3, 4, 5, 3, 'der']
+console.log('before unshift', num12)
+num12.unshift('mosm', 'exter', 'tss')
+console.log('after unshift', num12)
+num12.shift();
+console.log('after unshift', num12)
---
+++ June12/evenodd.ts
@@ -0,0 +1,5 @@
+function evenodd(a: number): boolean {
+    return a % 2 === 0
+}
+console.log(evenodd(3))
+console.log(evenodd(10))
